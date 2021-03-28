10d84827b51de746dea8dff7ee02a98863b10fde
Monica <tamang.monica@gmail.com>
2021-03-27 23:43:53 -0600

    Styled the battle page, added more images of pokemon and lightning bolts, re-wrote the 'attackMechanism' function, working on looping the process of the battle

--- js/app.js
+++ js/app.js
@@ -7,3 +7,10 @@ var userMaxHealth = Cookies.set(`userMaxHealth`, `1000`);
 var userCurrentHealth = Cookies.set(`userCurrentHealth`, `1000`);
 var computerMaxHealth = Cookies.set(`computerMaxHealth`, `1000`);
 var computerCurrentHealth = Cookies.set(`computerCurrentHealth`, `1000`);
+
+var cpuPokemonSelection = Cookies.set(`computerPokemon`, `Phantump`);
+
+// Cookies.set(`userMaxHealth`, `500`);
+// Cookies.set(`userCurrentHealth`, `500`);
+// Cookies.set(`computerMaxHealth`, `500`);
+// Cookies.set(`computerCurrentHealth`, `500`);
--- js/battle.js
+++ js/battle.js
@@ -1,32 +1,71 @@
-var computerPokemonCurrentHealth = Cookies.get(`computerCurrentHealth`);
-var computerHP = `<p>Computer Current Health: ${computerPokemonCurrentHealth}</p>`;
-document.getElementById(`pokemonTwo`).innerHTML = computerHP;
+// Getting the value of cookies based on the user's pokemon selection
+var userPokemonSelection = Cookies.get(`selection`);
+var computerPokemonSelection = Cookies.get(`computerPokemon`)
 
-var userPokemonCurrentHealth = Cookies.get(`userCurrentHealth`);
-Cookies.set(`userCurrentHealth`, userPokemonCurrentHealth);
-var userHP = `<p>User Current Health: ${userPokemonCurrentHealth}</p>`;
-document.getElementById(`pokemonOne`).innerHTML = userHP;
+// Creating a new image tag, styling the images, appending the image tag to the 'userPokemon'
+pokemonImage = document.createElement(`img`);
+pokemonImage.style.width = `100%`;
+pokemonImage.style.transform = `scaleX(-1)`;
+document.getElementById(`userPokemonImage`).append(pokemonImage);
+document.getElementById(`userPokemonImage`).style.width = `15vw`;
 
+// Printing the image of the user's pokemon to the DOM
+if (userPokemonSelection === `Turtwig`) {
+    pokemonImage.setAttribute(`src`, `/images/blackTurtwig.jpg`);
+} else if (userPokemonSelection === `Charmander`) {
+    pokemonImage.setAttribute(`src`, `/images/blackCharmander.jpg`);
+} else if (userPokemonSelection === `Piplup`) {
+    pokemonImage.setAttribute(`src`, `/images/blackPiplup.jpg`);
+}
+
+// Getting the user's current health cookies and printing it to the DOM, styling the user's health points
+var userCurrentHealth = Cookies.get(`userCurrentHealth`);
+var userCurrentHP = document.createElement(`p`);
+userCurrentHP.innerText = `${userCurrentHealth}/1000`;
+userCurrentHP.style.marginTop = `5vh`;
+document.getElementById(`userPokemon`).append(userCurrentHP);
+
+// Getting the computer's current health cookies and printing it to the DOM, styling the user's health points
+var computerCurrentHealth = Cookies.get(`computerCurrentHealth`);
+var computerCurrentHP = document.createElement(`p`);
+computerCurrentHP.innerText = `${computerCurrentHealth}/1000`;
+computerCurrentHP.style.marginTop = `5vh`;
+document.getElementById(`computerPokemon`).append(computerCurrentHP);
 
 function attackMechanism() {
-    computerPokemonCurrentHealth -= 200;
-    Cookies.set(`computerCurrentHealth`, computerPokemonCurrentHealth);
+    computerCurrentHealth -= 200;
+    Cookies.set(`computerCurrentHealth`, computerCurrentHealth);
+    computerCurrentHP.innerText = `${computerCurrentHealth}/1000`;
 
-    var computerHP = `<p>Computer Current Health: ${computerPokemonCurrentHealth}</p>`;
-    document.getElementById(`pokemonTwo`).innerHTML = computerHP;
+    if(computerCurrentHealth <= 0) {
+        computerCurrentHealth = 0;
+        Cookies.set(`computerCurrentHealth`, computerCurrentHealth);
+        computerCurrentHP.innerText = `${computerCurrentHealth}/1000`;
+        var winnerMessage = `<h1>${userPokemonSelection} Wins!</h1>`;
+        document.getElementById(`battleMessages`).innerHTML = winnerMessage;
+    }
 
-    if(computerPokemonCurrentHealth <= 0) {
-        computerPokemonCurrentHealth = 0;
-        var userPokemonWinner = Cookies.get(`selection`);
-        var winnerMessage = document.createElement(`p`);
-        winnerMessage.innerText = `${userPokemonWinner} Wins!`;
-        document.getElementById(`pokemonTwo`).append(winnerMessage);
-        Cookies.set(`computerCurrentHealth`, computerPokemonCurrentHealth);
-    } else {
-        var userPokemonCurrentHealth = Cookies.get(`userCurrentHealth`);
-        userPokemonCurrentHealth -= 200;
-        Cookies.set(`userCurrentHealth`, userPokemonCurrentHealth);
-        var userHP = `<p>User Current Health: ${userPokemonCurrentHealth}</p>`;
-        document.getElementById(`pokemonOne`).innerHTML = userHP;
+    userCurrentHealth -= 150;
+    Cookies.set(`userCurrentHealth`, userCurrentHealth);
+    userCurrentHP.innerText = `${userCurrentHealth}/1000`;
+
+    if(userCurrentHealth <= 0) {
+        userCurrentHealth = 0;
+        Cookies.set(`userCurrentHealth`, userCurrentHealth);
+        userCurrentHP.innerText = `${userCurrentHealth}/1000`;
+        var winnerMessage = `<h1>${computerPokemonSelection} Wins!</h1>`;
+        document.getElementById(`battleMessages`).innerHTML = winnerMessage;
     }
 }
+
+// if(gameOver) {
+//     userCurrentHealth = 1000;
+//     computerCurrentHealth = 1000;
+// } else {
+//     var restartGame = attackMechanism();
+// }
+
+// // var initialUserHP = 1000;
+// // for(userCurrentHealth === 0 || computerCurrentHealth === 0; userCurrentHealth <= 0 || computerCurrentHealth <= 0; userCurrentHealth = initialUserHP) {
+// //     var restartGame = attackMechanism();
+// // }
